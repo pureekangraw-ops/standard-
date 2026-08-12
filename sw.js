f9e49957346bf4f545cbb30357427ad45de7fdd7
pureekangraw-ops <pureekangraw@gmail.com>
2026-08-12 07:13:27 +0700

    chore: precache NormalPocket 1.1.0 catalog release

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const APP_CACHE_PREFIX = "ygph-standard-app-";
 const LEGACY_CACHE_PREFIXES = Object.freeze([
   "ygph-standard-0.1.0-preview."
 ]);
-const RELEASE_ID = "v1.0.0-20260809-r2-defrag-current";
+const RELEASE_ID = "v1.1.0-20260812-r3-product-catalog";
 const CURRENT_CACHE = `${APP_CACHE_PREFIX}${RELEASE_ID}`;
 const META_CACHE = "ygph-standard-meta";
 const META_PATH = "__ygph_service_worker_lifecycle__";
@@ -20,7 +20,12 @@ const APP_SHELL = [
   "metropolis-r5-2.css",
   "metropolis-r5-3.css",
   "metropolis-r5-4.css",
+  "normalpocket-products.css",
   "sw-bootstrap.js",
+  "normalpocket-bootstrap.js",
+  "normalpocket-catalog-core.js",
+  "normalpocket-products.js",
+  "normalpocket-reconcile.js",
   "highway-gate.js",
   "app.js",
   "flow-era.js",
