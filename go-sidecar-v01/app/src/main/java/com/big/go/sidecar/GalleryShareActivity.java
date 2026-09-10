b0c87e96eecfb596cfb303db051b822eb8568691
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-10 23:14:09 +0700

    feat: share up to ten gallery images

--- go-sidecar-v01/app/src/main/java/com/big/go/sidecar/GalleryShareActivity.java
+++ go-sidecar-v01/app/src/main/java/com/big/go/sidecar/GalleryShareActivity.java
@@ -8,7 +8,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.big.go.sidecar.core.GallerySelectionPolicy;
 import com.big.go.sidecar.core.GallerySharePolicy;
+
+import java.util.ArrayList;
 
 public final class GalleryShareActivity extends Activity {
     private static final int RC_PICK_IMAGE = 73;
@@ -22,7 +25,8 @@ public final class GalleryShareActivity extends Activity {
     private void openImagePicker() {
         Intent pick = new Intent(Intent.ACTION_OPEN_DOCUMENT)
                 .addCategory(Intent.CATEGORY_OPENABLE)
-                .setType(GallerySharePolicy.IMAGE_MIME);
+                .setType(GallerySharePolicy.IMAGE_MIME)
+                .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
         try {
             startActivityForResult(pick, RC_PICK_IMAGE);
         } catch (ActivityNotFoundException e) {
@@ -35,21 +39,55 @@ public final class GalleryShareActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode != RC_PICK_IMAGE) return;
-
-        Uri image = resultCode == RESULT_OK && data != null ? data.getData() : null;
-        if (image == null) {
+        if (resultCode != RESULT_OK || data == null) {
             finish();
             return;
         }
-        shareToChatGpt(image);
+
+        ArrayList<Uri> images = collectImages(data);
+        if (!GallerySelectionPolicy.isValidCount(images.size())) {
+            if (images.size() > GallerySelectionPolicy.MAX_IMAGES) {
+                Toast.makeText(this, "เลือกได้สูงสุด " + GallerySelectionPolicy.MAX_IMAGES + " รูปต่อครั้ง", Toast.LENGTH_LONG).show();
+            }
+            finish();
+            return;
+        }
+        shareToChatGpt(images);
     }
 
-    private void shareToChatGpt(Uri image) {
-        Intent share = new Intent(Intent.ACTION_SEND)
-                .setType(GallerySharePolicy.IMAGE_MIME)
-                .putExtra(Intent.EXTRA_STREAM, image)
-                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        share.setClipData(ClipData.newRawUri("GO gallery image", image));
+    private ArrayList<Uri> collectImages(Intent data) {
+        ArrayList<Uri> images = new ArrayList<>();
+        ClipData clip = data.getClipData();
+        if (clip != null) {
+            for (int i = 0; i < clip.getItemCount(); i++) {
+                Uri image = clip.getItemAt(i).getUri();
+                if (image != null && !images.contains(image)) images.add(image);
+            }
+        } else if (data.getData() != null) {
+            images.add(data.getData());
+        }
+        return images;
+    }
+
+    private void shareToChatGpt(ArrayList<Uri> images) {
+        String action = GallerySelectionPolicy.shareActionForCount(images.size());
+        Intent share;
+        if ("SEND_MULTIPLE".equals(action)) {
+            share = new Intent(Intent.ACTION_SEND_MULTIPLE)
+                    .setType(GallerySharePolicy.IMAGE_MIME)
+                    .putParcelableArrayListExtra(Intent.EXTRA_STREAM, images);
+        } else {
+            share = new Intent(Intent.ACTION_SEND)
+                    .setType(GallerySharePolicy.IMAGE_MIME)
+                    .putExtra(Intent.EXTRA_STREAM, images.get(0));
+        }
+        share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        ClipData grants = ClipData.newRawUri("GO gallery image", images.get(0));
+        for (int i = 1; i < images.size(); i++) {
+            grants.addItem(new ClipData.Item(images.get(i)));
+        }
+        share.setClipData(grants);
 
         Intent direct = new Intent(share).setPackage(GallerySharePolicy.CHATGPT_PACKAGE);
         try {
