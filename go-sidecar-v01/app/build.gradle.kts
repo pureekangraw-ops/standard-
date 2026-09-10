58246dfa4d9b675bba4f2ee96409f2f65252d84c
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-10 22:47:49 +0700

    build: bump GO Sidecar Dev to 0.1.5

--- go-sidecar-v01/app/build.gradle.kts
+++ go-sidecar-v01/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.big.go.sidecar.dev"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4-dev"
+        versionCode = 6
+        versionName = "0.1.5-dev"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
