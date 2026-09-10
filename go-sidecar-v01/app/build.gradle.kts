472ad8f1d716b727480db6692a889ca257d9adcc
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-10 09:09:06 +0700

    build: isolate Sidecar development package

--- go-sidecar-v01/app/build.gradle.kts
+++ go-sidecar-v01/app/build.gradle.kts
@@ -7,11 +7,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.big.go.sidecar"
+        applicationId = "com.big.go.sidecar.dev"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1-dev"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
