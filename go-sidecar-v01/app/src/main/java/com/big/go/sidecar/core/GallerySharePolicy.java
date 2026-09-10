467b9b6783ad5819b6e839304defc72f4c92709b
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-10 22:45:18 +0700

    feat: add direct gallery share policy

---
+++ go-sidecar-v01/app/src/main/java/com/big/go/sidecar/core/GallerySharePolicy.java
@@ -0,0 +1,10 @@
+package com.big.go.sidecar.core;
+
+public final class GallerySharePolicy {
+    public static final String CHATGPT_PACKAGE = "com.openai.chatgpt";
+    public static final String IMAGE_MIME = "image/*";
+    public static final boolean USES_SIDECAR_API = false;
+    public static final boolean USER_MUST_SEND = true;
+
+    private GallerySharePolicy() {}
+}
