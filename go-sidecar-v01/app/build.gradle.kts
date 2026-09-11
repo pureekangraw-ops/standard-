34f53782530f0e9c9e553850c75b535be15ffe80
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-11 14:34:32 +0700

    feat: route screenshots through GO room gates

--- go-sidecar-v01/app/build.gradle.kts
+++ go-sidecar-v01/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.big.go.sidecar.dev"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.1.6-dev"
+        versionCode = 8
+        versionName = "0.1.7-dev"
 
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
