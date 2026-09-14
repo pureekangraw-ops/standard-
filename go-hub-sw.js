d7312dc92d1cccad5e9fa6f7acc82a16948ff6b3
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-14 18:36:47 +0700

    fix: publish workbench model asset

--- go-hub-sw.js
+++ go-hub-sw.js
@@ -12,6 +12,7 @@ const APP_SHELL = [
   "./go-hub-runtime.js",
   "./go-hub-code-module.js",
   "./go-hub-code-task.js",
+  "./go-hub-workbench-model.js",
   "./go-hub-github-workspace.js",
   "./go-hub-persistence.js",
   "./go-hub-sw-bootstrap.js",
