5753fc5c21f7e97ccd4b722595a480391ed62729
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-16 03:13:16 +0700

    GO Hub: update go-hub-sw.js

--- go-hub-sw.js
+++ go-hub-sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "go-hub-app-";
-const CACHE_NAME = `${CACHE_PREFIX}v7-city-roundtrip`;
+const CACHE_NAME = `${CACHE_PREFIX}v7-centre-city-roundtrip`;
 const APP_SHELL = [
   "./",
   "./index.html",
