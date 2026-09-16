fd0c24702816058196d07db5a4c2987230c3a409
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-17 01:14:04 +0700

    GO Hub: update go-hub-sw.js

--- go-hub-sw.js
+++ go-hub-sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const CACHE_PREFIX = "go-hub-app-";
-const CACHE_NAME = `${CACHE_PREFIX}v7-route-optician-gate`;
+const CACHE_NAME = `${CACHE_PREFIX}v6-centre-live-checkpoint`;
 const APP_SHELL = [
   "./",
   "./index.html",
