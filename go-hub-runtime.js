4b884ed26d9c6153bc9bee4bc2c92cff6942a8e6
pureekangraw-ops <pureekangraw@gmail.com>
2026-09-17 20:24:48 +0700

    GO Hub: update go-hub-runtime.js

--- go-hub-runtime.js
+++ go-hub-runtime.js
@@ -1,6 +1,12 @@
 export { createTrafficSummary, createTrafficSnapshot } from "./go-hub-traffic.js";
 export { createTrafficDashboard, getTrafficDashboardStation } from "./go-hub-dashboard-model.js";
-export { createFactoryStationMonitor, createLibraryStationMonitor, createVerificationStationMonitor } from "./go-hub-station-monitors.js";
+export {
+  createFactoryStationMonitor,
+  createLibraryStationMonitor,
+  createLibraryTrafficSummary,
+  createVerificationStationMonitor,
+  createVerificationTrafficSummary,
+} from "./go-hub-station-monitors.js";
 
 export function createHubRuntime() {
   const entries = new Map();
